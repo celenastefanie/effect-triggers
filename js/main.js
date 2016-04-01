6b6536e60d223ea0a8810357e5f1ed177102d2f8
celenastefanie <celenastefanie@gmail.com>
2016-04-01 17:05:11 -0400

    im so triggered rn ~

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var $box = $('.box');
+var $btnShowHide = $('.btn-show-hide');
+var $diamond = $('.diamond');
+var $btnMoveInOut = $('.btn-move');
+var $panel = $('.panel');
+var $btnCollapse = $('.btn-collapse-expand');
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
+
+$btnShowHide.on('click', function (e) {
+  $box.toggleClass('is-clicked');
+});
+
+$btnMoveInOut.on('click', function (e) {
+  $diamond.toggleClass('diamond-clicked');
+});
+
+$btnCollapse.on('click', function (e) {
+  $panel.toggleClass('panel-clicked');
+});
+
+$btnBounce.on('click', function (e) {
+  $circle.toggleClass('circle-clicked');
+});
+
+$btnBounce.on('animationend', '.circle-clicked', function () {
+  $(this).removeClass();
+});
+
+$btnAppend.on('click', function (e) {
+  var $li = $('<li>');
+  
+  $list.append($li);
+});
